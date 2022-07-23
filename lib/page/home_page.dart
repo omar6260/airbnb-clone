900f924ca18341c468f404f3bf231f10d246d893
Oumar Fall <foumar404@gmail.com>
2022-07-23 19:15:45 +0100

    je complété l'app

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/city_widgets.dart';
+import '../widgets/fade_app_bar.dart';
+import '../widgets/header.dart';
 import '../widgets/hero_banner.dart';
+import '../widgets/live_anywhere_widgets.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late ScrollController _scrollController;
+  double _scrollControllerOffset = 0.0;
+
+  void _scrollListener() {
+    setState(() {
+      _scrollControllerOffset = _scrollController.offset;
+    });
+  }
+
+  /// > The initState() function is called when the widget is first created
+  @override
+  void initState() {
+    /// Creating a scroll controller and adding a listener to it.
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          const CustomScrollView(
+          CustomScrollView(
             slivers: [
-              HeroBanner()
+              HeroBanner(),
+              Header("Explore Nearby"),
+              CitiesGrid(),
+              LiveAnywhereList(),
             ],
           ),
+          FadeAppBar(scrollOffset: _scrollControllerOffset),
         ],
       ),
     );
